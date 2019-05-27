bbc259d55c4900a403aebe956326ef0c324df854
JakubR1999 <50864649+JakubR1999@users.noreply.github.com>
2019-05-27 21:58:31 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,37 @@
 var canvas = document.querySelector("#canvas");
 var ctx = canvas.getContext('2d');
 
-function blok() {
+var SPACE = 32;
+var LEFT = 37;
+var RIGHT = 39;
+
+function game(){
+	this.size = {
+		gameWidth: 750,
+		gameHeight: 750
+	};
+
+	this.lives = 3;
+	this.score = 0;
+	this.level = 1;
+	
+}
+
+function start(){
+	ctx.clearRect( 0, 0, gameWidth, gameHeight);
+	ctx.font = "50px Arial";
+	ctx.fillStyle = '#ffffff';
+	ctx.textBaseline = "center";
+	ctx.textAlign = "center";
+	ctx.fillText("Space Invaders", gameWidth / 2 , gameHeight / 2);
+	ctx.font = "25px Arial";
+	ctx.fillText("Press 'Space' to start the game.", gameWidth / 2 , gameHeight / 2 - 80);
+	
+};
+
+start();
+
+function draw() {
 	ctx.beginPath();
 	ctx.moveTo(25, 600);
 	ctx.lineTo(25, 583);
@@ -24,8 +54,12 @@ function blok() {
 	ctx.closePath();
 	ctx.stroke();
 }
-blok();
-const image = new Image(900, 520);
-image.src = 'alien_1.jpg';
-ctx.drawImage( image, 0, 0);
+
+document.addEventListener("keydown", keySpace);
+
+function keySpace(e){
+	if( e == SPACE ){
+		ctx.clearRect( 0, 0, gameWidth, gameHeight);
+	}
+};
 	
